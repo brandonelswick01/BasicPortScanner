0e03b818902055f9ae1af0bb89cf4ea53b3b1916
Brandon Elswick <144645445+brandonelswick01@users.noreply.github.com>
2026-03-27 12:16:38 -0400

    Update portscanner.go

--- portscanner.go
+++ portscanner.go
@@ -9,7 +9,7 @@ func main() {
 	fmt.Printf("This is a Basic SOC Analysis Project\n")
 	fmt.Printf("We are going to be seeing if Google has port 80 open\n")
 	
-	host := "www.cia.gov" //Target Host
+	host := "www.google.com" //Target Host
 	port := "80" //Port to Scan
 	
 	address := fmt.Sprintf("%s:%s", host, port)
